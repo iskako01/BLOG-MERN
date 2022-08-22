5ff43a18191b9df37cf16ba30055be1c10a2a487
iskako01 <85966697+iskako01@users.noreply.github.com>
2022-08-22 20:37:44 +0200

    Added: upload file

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import multer from "multer";
 import mongoose from "mongoose";
 import {
   loginValidation,
@@ -6,6 +7,7 @@ import {
   postValidation,
 } from "./validations/validations.js";
 import checkAuth from "./utils/checkAuth.js";
+import handleValidationErrors from "./utils/handleValidationErrors.js";
 import { url, port } from "./const.js";
 import { login, register, getMe } from "./controllers/UserController.js";
 import {
@@ -18,19 +20,49 @@ import {
 
 const app = express();
 
+const storage = multer.diskStorage({
+  destination: (_, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
 app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 
 //Auth
-app.post("/auth/login", loginValidation, login);
-app.post("/auth/register", registerValidation, register);
-app.get("/auth/me", checkAuth, getMe);
+app.post("/auth/login", loginValidation, handleValidationErrors, login);
+app.post(
+  "/auth/register",
+  registerValidation,
+  handleValidationErrors,
+  register
+);
+app.get("/auth/me", handleValidationErrors, checkAuth, getMe);
+
+//Upload image
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
 
 //Post
 app.get("/posts", getAllPosts);
 app.get("/posts/:id", getOnePost);
-app.post("/posts", checkAuth, postValidation, createPost);
+app.post(
+  "/posts",
+  checkAuth,
+  postValidation,
+  handleValidationErrors,
+  createPost
+);
 app.delete("/posts/:id", checkAuth, removePost);
-app.patch("/posts/:id", checkAuth, updatePost);
+app.patch("/posts/:id", checkAuth, handleValidationErrors, updatePost);
 
 app.listen(port, (err) => {
   if (err) {
