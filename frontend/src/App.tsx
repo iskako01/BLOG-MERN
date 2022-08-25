310b0e5b044f4cf780607103b290c02f344b6e61
iskako01 <85966697+iskako01@users.noreply.github.com>
2022-08-25 09:51:42 +0200

    Added: pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Container from "@mui/material/Container";
+import "./App.css";
+import { HomePage } from "./pages/HomePage";
+import { FullPost } from "./pages/FullPost";
+import { AddPost } from "./pages/AddPost/AddPost";
+import { Login } from "./pages/Login/Login";
+import { Registration } from "./pages/Registration/Registration";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="lg">
+      <HomePage />
+      <FullPost />
+      <AddPost />
+      <Login />
+      <Registration />
+    </Container>
   );
 }
 
---
+++ frontend/src/pages/FullPost.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const FullPost = () => {
+  return <div>FullPost</div>;
+};
